7aaca49e53315a59499a7bca7ca3e9348e40b380
Yuliia Nykanorova <juliyasos@ukr.net>
2025-08-01 17:53:18 +0300

    added README, fixed script for burger menu, modified hover styles of buttons

---
+++ README.md
@@ -0,0 +1,34 @@
+# Diagnostic Center
+This is a learning project built for practice and portfolio purposes. A fully responsive landing page based on a design concept, implemented with semantic HTML, modular SCSS, and vanilla JavaScript.
+
+**Live Demo**
+[View Live on Vercel](https://hw-16-diagnostic-center.vercel.app/)
+
+## Features
+- Built with semantic HTML5, SCSS, and vanilla JavaScript;
+- Responsive design for mobile, tablet, and desktop;
+- Interactive burger menu for mobile devices;
+- Carousel slider implemented with Owl Carousel.
+
+## Getting Started
+### 1) Clone the repository
+```bash
+git clone https://github.com/yu-nykanorova/hw-16-diagnostic-center.git
+cd hw-16-diagnostic-center
+```
+### 2) Open the project
+You can open the index.html file directly in your browser or use a local server such as **Live Server** in VS Code.
+
+## Technologies Used
+- HTML5 (semantic layout)
+- SCSS (modular and responsive styling)
+- Vanilla JavaScript (interactive elements)
+- Owl Carousel (responsive slider)
+
+## Design
+The layout is fully based on the following [Figma mockup](https://www.figma.com/design/qIregiEt35clBx5MWM7eQD/Free-Diagnostic-Center-Landing-Page-Design--Community---Copy-?node-id=1-16&p=f&t=1ieaZRpvVhxUmuIY-0)
+
+## License
+This project is open source and free to use for learning, experimenting, or portfolio demonstration.
+
+*Created by Yuliia Nykanorova*
--- script.js
+++ script.js
@@ -1,19 +1,48 @@
 const menu = document.querySelector("#headerUl");
 const burgerButton = document.querySelector("#burgerButton");
 const closeButton = document.querySelector("#closeButton");
+const form = document.querySelector(".header-form");
+const input = form.querySelector(".header-form__input");
+const body = document.body;
 
-function menuToggle() {
-	menu.classList.toggle("_active");
+// For burger menu
+
+function openMenu() {
+	menu.classList.add("_active");
+	body.classList.add("no-scroll");
 }
 
-burgerButton.addEventListener("click", menuToggle);
-closeButton.addEventListener("click", menuToggle);
+function closeMenu() {
+	menu.classList.remove("_active");
+	body.classList.remove("no-scroll");
+}
+
+function isMenuOpen() {
+	return menu.classList.contains("_active");
+}
+
+burgerButton.addEventListener("click", openMenu);
+closeButton.addEventListener("click", closeMenu);
 
 document.addEventListener("click", (e) => {
-	if (!menu.contains(e.target) && !e.target.closest("#burgerButton")) {
-		menuToggle();
+	const clickOutsideMenu = !menu.contains(e.target) && !e.target.closest("#burgerButton");
+	
+	if (isMenuOpen() && clickOutsideMenu) {
+		closeMenu();
 	}
 });
+
+// For for header-form styles
+
+input.addEventListener("input", () => {
+	if (input.value) {
+		form.classList.add("filled");
+	} else {
+		form.classList.remove("filled");
+	}
+});
+
+//For owl carousel
 
 $(document).ready(function () {
 	$(".owl-carousel").owlCarousel({
